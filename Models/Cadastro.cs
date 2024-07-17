ae614f3756ff45736b4eb15615f6d67a4b9dfc5c
Gabriel Dias <gabrielsonicdias@gmail.com>
2024-07-17 19:41:50 -0300

    First Commit

---
+++ Models/Cadastro.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SistemaIgreja.Models
+{
+    public class Cadastro
+    {
+        [Column("Id")]
+        [Display(Name = "Matrícula")]
+        public int Id { get; set; }
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; } = string.Empty;
+    }
+}
---
+++ Models/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SistemaIgreja.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        { 
+
+        }
+
+        public DbSet<Cadastro> Cadastro { get; set; }
+
+
+    }
+}
